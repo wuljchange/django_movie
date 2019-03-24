2b92b209c6ba6918e193e41fbc6af52c83e40fc1
wuljchange <wuljchange@outlook.com>
2019-03-24 21:18:44 +0800

    [feat] 优化登录和注册页面

--- movies/forms.py
+++ movies/forms.py
@@ -3,7 +3,11 @@ from django import forms
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(label='用户名')
+    email = forms.EmailField(label='邮件地址')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='密码')
+    username.widget.attrs['class'] = 'form-control'
+    email.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = User
